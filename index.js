257413d0e892adf98d54ac7b28c86754e96bbf98
GytisKupinas <gytis.kupinas@gmail.com>
2021-06-08 13:23:38 +0300

    update

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-console.log("Hello World");
+var sk1;
+var sk2;
+          // galima naudoti kintamuosius nuo a-z A-Z $ _ 0-9
+sk1 = 5;
+sk2 = 7;
+            // KAMEL rasymo budas zodziu pavyzdys pradeda mazosiomis sekantys zodziai is didziosiomis  asEinuMiegoti
+var rez;
+
+rez = sk1 + sk2;
+//     5 + 7        yra 2 tipu komentarai //-eiluteje visoje raso ir /* */- raso viduje iki uzdarumu skliaustu
+/*
+pasiziuriu kas yra saugoma sk1 ->5
+pasiziuriu kas yra saugoma sk2 ->7
+atliekam sudeti 5+7=12
+12 -> rez
+
+var - kintamojo deklaravimas
+  = - reiksmes priskyrimas kintamajam
+  + - sudetis 
+  - - atimtis
+  / - dalyba
+  * - daugyba
+*/
+
+
+
+console.log(rez);

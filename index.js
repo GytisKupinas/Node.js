e577c132974934206fd213f8fa0b1f0a4496bc36
GytisKupinas <gytis.kupinas@gmail.com>
2021-06-09 20:36:57 +0300

    up

--- index.js
+++ index.js
@@ -7,15 +7,15 @@ var sk2;
 var bendarSaskaitosSum;
 var TotalInvoiceSum       // KAMEL rasymo budas zodziu pavyzdys pradeda mazosiomis sekantys zodziai is didziosiomis  asEinuMiegoti
 
-sk1 = 5;
-sk2 = 7;
+sk1 = 0.5;
+sk2 = 0.7;
 
-var sk3 = 4;
-sk3 = 2;
+var sk3 = 0.2;
+
             
 var rez;
 
-rez = sk1 + sk2 / sk3 * sk1;
+rez = sk1 + sk2 * sk3 / sk1;  // ima pagal tai kuiris veiksmas sturi aukstesni lygi, ziureti puslapyje, ta veiksma pirma ir atlikines. taisykles is desines i kaire ar is kaires i desine .
 
                           //     5 + 7        yra 2 tipu komentarai //-eiluteje visoje raso ir /* */- raso viduje iki uzdarumu skliaustu
                           /*
@@ -31,6 +31,29 @@ rez = sk1 + sk2 / sk3 * sk1;
                             * - daugyba
                             */
            //paimam reiksme is rez ->12. paimam reiksme is sk3 -> 4. padalinam 12 / 4 = 3. padedam gauti reiksme i rez
+           /*
+
+0000 0 0            
+0001 1 1
+0010 2 2
+0011 3 3
+0100 4 4
+0101 5 5
+0110 6 6
+0111 7 7
+1000 8 -8
+1001 9 -7
+1010 10 -6
+1011 11 -5
+1100 12 -4
+1101 13 -3
+1110 14 -2
+1111 15 -1
+
+2^10 - kilo
+2^20 = 2^10 * 2^10 - mega
+2^30 = 2^10 * 2^10 * 2^10 - giga
+           */
 
 
 

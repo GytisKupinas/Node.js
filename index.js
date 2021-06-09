4bd51a18bb6891901935331cf8dd64ffebc54247
GytisKupinas <gytis.kupinas@gmail.com>
2021-06-09 19:01:37 +0300

    up

--- index.js
+++ index.js
@@ -1,26 +1,36 @@
+"use strict";
+ 
 var sk1;
 var sk2;
-          // galima naudoti kintamuosius nuo a-z A-Z $ _ 0-9
+// galima naudoti kintamuosius nuo a-z A-Z $ _ 0-9
+
+var bendarSaskaitosSum;
+var TotalInvoiceSum       // KAMEL rasymo budas zodziu pavyzdys pradeda mazosiomis sekantys zodziai is didziosiomis  asEinuMiegoti
+
 sk1 = 5;
 sk2 = 7;
-            // KAMEL rasymo budas zodziu pavyzdys pradeda mazosiomis sekantys zodziai is didziosiomis  asEinuMiegoti
+
+var sk3 = 4;
+sk3 = 2;
+            
 var rez;
 
-rez = sk1 + sk2;
-//     5 + 7        yra 2 tipu komentarai //-eiluteje visoje raso ir /* */- raso viduje iki uzdarumu skliaustu
-/*
-pasiziuriu kas yra saugoma sk1 ->5
-pasiziuriu kas yra saugoma sk2 ->7
-atliekam sudeti 5+7=12
-12 -> rez
+rez = sk1 + sk2 / sk3 * sk1;
 
-var - kintamojo deklaravimas
-  = - reiksmes priskyrimas kintamajam
-  + - sudetis 
-  - - atimtis
-  / - dalyba
-  * - daugyba
-*/
+                          //     5 + 7        yra 2 tipu komentarai //-eiluteje visoje raso ir /* */- raso viduje iki uzdarumu skliaustu
+                          /*
+                          pasiziuriu kas yra saugoma sk1 ->5
+                          pasiziuriu kas yra saugoma sk2 ->7
+                           atliekam sudeti 5+7=12
+                           12 -> rez
+                          var - kintamojo deklaravimas
+                           = - reiksmes priskyrimas kintamajam
+                           + - sudetis 
+                           - - atimtis
+                             / - dalyba
+                            * - daugyba
+                            */
+           //paimam reiksme is rez ->12. paimam reiksme is sk3 -> 4. padalinam 12 / 4 = 3. padedam gauti reiksme i rez
 
 
 

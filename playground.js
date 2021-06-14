2497cfcfb8ae8c31812f892c3e626b37581fe4de
GytisKupinas <gytis.kupinas@gmail.com>
2021-06-14 22:25:08 +0300

    up

--- playground.js
+++ playground.js
@@ -77,12 +77,62 @@ while (i <= iki) {
     i = i +1;
 }
 */
+
+
 // suskaiciuoti visu skaiciu nuo 1 iki 100 , kurie dalinasi is 3 suma
+
+/* var suma = 0;
+
+  var i = 1;
+
+while (i <= 100) {
+    if (i % 3 === 0) {
+        suma = suma +i;
+    }
+    i = i + 1;
+}
+
+console.log(suma);
+*/
+
 
 
 
 // atspausdinti visus pirminius skaicius intervale nuo 2 iki 100
 
+ /*var i = 2;
+ 
+while (i <= 100) {
+ var arPirminis = true;
+ var atgal = i - 1;
+ while (atgal >= 2 && arPirminis) {
+ if (i % atgal === 0) {
+ arPirminis = false;
+ }
+ atgal = atgal - 1;
+ }
+ if (arPirminis) {
+ console.log(i);
+ }
+ i = i + 1;
+}
+*/
+
+// namu darbas daugybos lentele. var nuo = 1;  var iki = 4;
+
+var nuo = 1;
+var iki = 3;
+var i = nuo;
+while (i <= iki) {
+    var j = nuo;
+    while (j <= iki) {
+        console.log(i * j);
+        j += 1;
+    }
+    i += 1;
+}
+console.log("Pabaiga");
 
 
-console.log("pabaiga");
+
+

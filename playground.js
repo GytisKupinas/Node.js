374b41f63b1aa16d6c81f1dbd3278d94f03316e9
GytisKupinas <gytis.kupinas@gmail.com>
2021-06-14 21:44:16 +0300

    up

--- playground.js
+++ playground.js
@@ -52,13 +52,29 @@ while (sk <= 100) {
 }
 */
 
-var sk = 1;
+/* var sk = 1;
 while (sk <= 100) {
     if (sk % 5 === 0 || sk % 2 === 0) {
         console.log(sk);
         }
          sk = sk + 1;
 }
+*/
 
+
+
+
+var nuo = 1;
+var iki = 3;
+
+var i = 1;
+while (i <= iki) {
+    var j = 1;
+    while (j <= iki) {
+        console.log(i * j);
+        j = j + 1;
+    }
+    i = i +1;
+}
 
 console.log("pabaiga");

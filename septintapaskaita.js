7b7c31937d5b0a6dc23b7d48efa44ba9e53a557a
GytisKupinas <gytis.kupinas@gmail.com>
2021-06-16 12:49:49 +0300

    up

--- septintapaskaita.js
+++ septintapaskaita.js
@@ -1,4 +1,4 @@
-console.log("pradzia");
+/*console.log("pradzia");
 {
 let sk = 3;
 console.log("komanda bloke", sk);
@@ -12,3 +12,21 @@ console.log("kita komanda bloke", sk);
 }
 console.log(sk);
 console.log("pabaiga");
+*/
+
+console.log("pradzia");
+ 
+{
+ let sk = 3;
+ console.log("komanda bloke", sk);
+ {
+ let sk = 7;
+ let kitas = 5;
+ console.log("komanda vidiniam bloke");
+ sk = sk * kitas;
+ console.log("komanda 2 vidiniam bloke", sk, kitas);
+ }
+ console.log(sk);
+ sk = 17;
+ console.log("kita komanda bloke", sk);
+}
